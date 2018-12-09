cbe7210ada64a3621b99150df7ff7d8110388b0d
jkl1008 <544314759@qq.com>
2018-12-09 11:49:42 +0800

    自己关注知乎好友

--- zhihu.py
+++ zhihu.py
@@ -1,5 +1,12 @@
+from selenium import webdriver
+import time
+
+
+
 def start_chrome():
-    pass
+    driver = webdriver.Chrome(executable_path='./chromedriver.exe')  # 实例化浏览器驱动实例
+    driver.start_client()  # 打开客户端
+    return driver
 
 def find_strangers():
     pass
